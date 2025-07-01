2f70264eeb46b589f972d3c600d6d16b86deb340
Davi Aravechia <davi.aravechia@aluno.fmpsc.edu.br>
2025-07-01 01:28:25 -0300

    Quiz mecanina completa

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,14 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Welcome from './components/quiz/Welcome.jsx'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import { QuizContext } from './context/quiz.jsx'
+// import { useContext } from 'react'
+// import Question from './components/quiz/Questions.jsx'
+// import './App.css'
+// import QuizHome from './components/quiz/QuizHome'
+// import { QuizProvider } from './context/quiz.jsx'
 
-function App()  {
+// function App()  {
+//   const [quizState, dispatch] = useContext(QuizContext);
+
+//   return (
+//     <div className="App">
+//       <h1>Bem vindo</h1>
+//       {quizState.gameStage === 'Start' && <QuizHome />}
+//       {quizState.gameStage === 'Playing' && <Question />}
+        
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React, { useEffect } from 'react'
+import { use, useContext } from 'react'
+import { QuizContext } from './context/quiz.jsx'
+import './App.css'
+import { QuizProvider } from './context/quiz.jsx'
+
+import EndGame from './components/quiz/EndGame.jsx'
+import QuizHome from './components/quiz/QuizHome'
+import Question from './components/quiz/Questions.jsx'
+
+function QuizContent() {
+  const [quizState, dispatch] = useContext(QuizContext)
+  
+  useEffect(() => {
+    //embaralhar perguntas
+    dispatch({type: 'REORDER_QUESTIONS'})
+  }, [])
+  return (
+    <>
+      <h1>Bem vindo</h1>
+      {quizState.gameStage === 'Start' && <QuizHome />}
+      {quizState.gameStage === 'Playing' && <Question />}
+      {quizState.gameStage === 'End' && <EndGame />} 
+      {/* {quizState.gameStage === 'NEW_GAME' && <EndGame />}  */}
+    </>
+  )
+}
+
+function App() {
+
   return (
     <div className="App">
-      <h1>Quiz sobre a FMP</h1>
-      <Welcome />
+      <QuizProvider>
+        <QuizContent />
+      </QuizProvider>
     </div>
   )
 }
